c9fdc25d51926622f3f7e6524ad923fc08ccbf2e
davemackintosh <me@davemackintosh.co.uk>
2016-02-23 11:42:51 +0000

    Tests and coverage.
    Signed-off-by: davemackintosh <me@davemackintosh.co.uk>

--- tests/all.js
+++ tests/all.js
@@ -4,7 +4,20 @@ const tape = require("tape")
 const Node_slack = require("../index")
 
 tape("All tests", test => {
-  const instance = new Node_slack("123")
+  test.plan(3)
 
-  console.log(instance.channels.archive.toString())
+  // Create an instance.
+  const instance = new Node_slack(process.env.SLACK_TEST_ACCESS_TOKEN.toString())
+
+  instance.api.test((err, res) => {
+    test.equal(res.ok, true, "Got okay from the Slack API.")
+  })
+
+  instance.api.test({ foo: "bar" }, (err, res) => {
+    test.equal(res.ok, true, "Got okay from the Slack API with params.")
+  })
+
+  instance.usergroups.users.list({ usergroup: "S0604QSJC" }, (err, res) => {
+    test.ok(res, "Got a response from the Slack API on a sub-method.")
+  })
 })
