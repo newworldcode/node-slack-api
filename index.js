c23e613aecb32bb26e9bb6c5acce991302006fe7
davemackintosh <me@davemackintosh.co.uk>
2016-02-23 09:40:33 +0000

    Annotations and callback instead of throw.
    Signed-off-by: davemackintosh <me@davemackintosh.co.uk>

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const endpoints = require("./lib/api")
 
 class Slack {
   constructor(token) {
+    // Get the method groups.
     Object
       .keys(endpoints)
       .forEach(method_group => {
@@ -16,14 +17,19 @@ class Slack {
         Object
           .keys(endpoints[method_group])
           .forEach(method_name => {
+            // Register the function with parameter validation.
             this[method_group][method_name] = (params, callback) => {
+              // Check the order of the arguments, some endpoints
+              // don't take parameters.
               if (!callback && params instanceof Function) {
                 return utils.call_slack_api(token, `${method_group}.${method_name}`, callback)
               }
               else {
+                // Validate the incoming parameters with
+                // the schema in `lib/api.js`.
                 Joi.validate(params, endpoints[method_group][method_name], (err, values) => {
                   if (err) {
-                    throw err
+                    callback(err)
                   }
                   else {
                     return utils.call_slack_api(token, `${method_group}.${method_name}`, values, callback)
